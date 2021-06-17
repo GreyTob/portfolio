9adaec64d227f98dab1baf6311496ec0d7c5a2c8
Sergey <GreyTob@inbox.ru>
2021-06-17 20:06:35 +0500

    works

---
+++ js/sliderWorks.js
@@ -0,0 +1,55 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+
+const container = document.querySelector('.works')
+const sidebar = document.querySelector('.sidebar')
+
+const mainSlide = document.querySelector('.main-slide')
+const slideCount = mainSlide.querySelectorAll('div').length
+
+//переменная для того что бы контролировать какой сейчас слайд
+let activeSlideIndex = 0
+
+//количество слайдов -1
+sidebar.style.top = `-${(slideCount - 1) * 100}vh`
+
+upBtn.addEventListener('click', () => {
+  changeSlide('up')
+})
+downBtn.addEventListener('click', () => {
+  changeSlide('down')
+})
+
+//переключения по скролу и кнопкам отключил, из-за fullSkroll
+// document.addEventListener('keydown', (event) => {
+//   // console.log(event.key)
+//   if (event.key === 'ArrowUp') {
+//     changeSlide('up')
+//   } else if (event.key === 'ArrowDown') {
+//     changeSlide('down')
+//   }
+// })
+
+// window.addEventListener('mousewheel', () => {
+//   changeSlide('up')
+// })
+
+function changeSlide(direction) {
+  if (direction === 'up') {
+    activeSlideIndex++
+    //если activeSlideIndex равен количеству сладов, то обнулить
+    if (activeSlideIndex === slideCount) {
+      activeSlideIndex = 0
+    }
+  } else {
+    activeSlideIndex--
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slideCount - 1
+    }
+  }
+
+  const height = container.clientHeight
+
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+}
