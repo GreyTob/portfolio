5b1c1879fc0f5e7bb551595a0bf8fcd8d025fcdd
Sergey <GreyTob@inbox.ru>
2021-06-25 20:04:10 +0500

    first deploy

--- js/formToMail.js
+++ js/formToMail.js
@@ -1,20 +1,23 @@
-$(document).ready(function() {
+document.querySelector('#formSend').onclick = (e) => {
+  e.preventDefault()
+}
 
-	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-});
+$(document).ready(function () {
+  //E-mail Ajax Send
+  $('form').submit(function () {
+    //Change
+    var th = $(this)
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php', //Change
+      data: th.serialize(),
+    }).done(function () {
+      alert('Thank you!')
+      setTimeout(function () {
+        // Done Functions
+        th.trigger('reset')
+      }, 1000)
+    })
+    return false
+  })
+})
